f7e1c29060dbe3459b3c20696710b42fc17391af
David Cisneros <d.cisneros849@gmail.com>
2016-06-05 14:34:12 -0400

    Created learn method and teach method for student and instructor class, respectively

--- people.rb
+++ people.rb
@@ -1,5 +1,13 @@
 class Student
+
+  def learn
+    puts "I get it!"
+  end #end learn method
 end #end for Student class
 
 class Instructor
-end #end for Instructor class 
+
+  def teach
+    puts "Everything in Ruby is an Object"
+  end
+end #end for Instructor class
